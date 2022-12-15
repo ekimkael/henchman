1f52f880ed98871b6931e5799e826b22f2885f1c
ekimkael <michelmbili@gmail.com>
2022-12-15 11:34:28 +0100

    installing required dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
 import App from "./App"
-import "./index.css"
+import React from "react"
+import * as ReactDOM from "react-dom/client"
+import { ChakraProvider } from "@chakra-ui/react"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+// Create a client
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<ChakraProvider>
+				<App />
+			</ChakraProvider>
+		</QueryClientProvider>
 	</React.StrictMode>
 )
